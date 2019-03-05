d85cc816f5995727cb91f30c6c9e4973f64c6ff8
Jakob Jul Elben <elbenjakobjul@gmail.com>
2019-03-05 14:46:36 +0100

    Creating my first package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='hello',
+      version='0.1',
+      description='Helloing the world',
+      url='https://github.com/elben10/minimalpythonpackage',
+      author='Jakob Jul Elben',
+      author_email='hello@hello.com',
+      license='MIT',
+      packages=['hello'],
+      zip_safe=False)
